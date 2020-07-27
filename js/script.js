9712f559484903c81c04df3b22edcba20d3a8cf1
gaizenband <gazik211@gmail.com>
2020-07-27 13:46:43 +0300

    more animations

--- js/script.js
+++ js/script.js
@@ -13,6 +13,44 @@ function initCarouselIndicators() {
     });
 }
 
+function sliding() {
+    let slider = document.querySelector(".header");
+    let bg = document.querySelector(".bg");
+    let sliderWidth = slider.offsetWidth;
+    let slideeHeight = slider.offsetHeight;
+    let bgMovement = 40;
+
+    slider.addEventListener("mousemove", (event) => {
+    movement(event);
+    });
+    function movement(event) {
+    let mouseX = Math.floor(
+        -(event.offsetX - sliderWidth / 2) * (bgMovement / sliderWidth)
+    );
+    let mouseY = Math.floor(
+        -(event.offsetY - slideeHeight / 2) * (bgMovement / slideeHeight)
+    );
+    bg.style.transform = `translate(${mouseX}px,${mouseY}px)`;
+    }
+}   
+
+const btn = () => {
+    let button = document.querySelector('.playBtn');
+    console.log(button);
+    button.addEventListener('click', function(){videoPlay();});
+}
+
+const videoPlay = () => {
+    let video = document.querySelector('.video');
+    let materials = document.querySelector('.video_materials');
+    video.play();
+    video.setAttribute('controls', true);
+    materials.style.display = 'none';
+}
+
 $(document).ready(function () {
     initCarouselIndicators();
+    sliding();
+    btn();
 })
+
